82b6048cf3fa77dbd076845ea7e2df44ced3d562
Thamburaan <jaisejohnk@gmail.com>
2017-10-21 21:01:25 -0400

    fun commit agaain.....
    hahhaa

---
+++ Spring5App1/src/main/java/com/jaiseSpring5/Spring5App1/Spring5App1Application.java
@@ -0,0 +1,12 @@
+package com.jaiseSpring5.Spring5App1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5App1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5App1Application.class, args);
+	}
+}
